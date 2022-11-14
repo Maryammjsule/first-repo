77f98fde2a69054988445ab96092ac07a0a876e3
Maryammjsule <106980029+Maryammjsule@users.noreply.github.com>
2022-11-14 12:57:56 +0000

    commit

---
+++ newfile1.py
@@ -0,0 +1,6 @@
+prompt = '\ntell me your problem amd i wl provide a solution'
+prompt += "\nenter 'done' to end this program"
+message = ' '
+while message != 'done':
+	      message = input(prompt)
+	      print(message)
